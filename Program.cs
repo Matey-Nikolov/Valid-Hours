2824bec368c33fe3cfb5285ed67d896dc982bb74
Matey-Nikolov <matey.nikolov@mail.bg>
2021-03-23 18:12:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ValidHours
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            string hour = Console.ReadLine();
+
+            Regex regex = new Regex(@"(([0-9])|([0-1][0-9])|(2[0-3])):([0-5][0-9]):([0-5][0-9])", RegexOptions.IgnoreCase);
+
+            MatchCollection matchHours = regex.Matches(hour);
+
+            foreach (Match validHour in matchHours)
+            {
+                if (validHour.Success)
+                {
+                    Console.WriteLine($"True: {validHour.Groups[0].Value}");
+                }
+            }
+        }
+    }
+}
